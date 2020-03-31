f3d03ae6e81a479baca4da78785f5f6e05f6e7c4
MirkoPralica <56869159+MirkoPralica@users.noreply.github.com>
2020-03-31 15:01:37 +0200

    @MirkoPralica, Added testing of printing GameBoard'

--- Source/Ludo/Program.cs
+++ Source/Ludo/Program.cs
@@ -7,7 +7,20 @@ namespace Ludo
     {
         static void Main(string[] args)
         {
-            
+            GameBoard gameBoard = new GameBoard();
+            int horizontal = gameBoard.Placements.GetUpperBound(0);
+            int vertical = gameBoard.Placements.GetUpperBound(0);
+
+            for (int i = 0; i <= horizontal; i++)
+            {
+                for (int j = 0; j <= vertical; j++)
+                {
+
+                    string res = gameBoard.Placements[i, j];
+                    Console.Write(res + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
