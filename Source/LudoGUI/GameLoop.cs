e18a8df006875d5aa79657feebda0c584adc885f
Hampus Kjellstrand <hampe.kjellstrand@gmail.com>
2020-04-08 17:02:07 +0200

    @Gerglamesh, rebase master commit

---
+++ Source/LudoGUI/GameLoop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EngineClasses;
+
+namespace LudoGUI
+{
+    public static class GameLoop
+    {
+        public static void StartRound(GameEngine gameEngine)
+        {
+            Player currentPlayer = gameEngine.CurrentPlayerTurn();
+
+        }
+    }
+}
